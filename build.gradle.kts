284af7014c6c026610e11addbdec07b3e87996cf
caoccao <caoccao@users.noreply.github.com>
2023-11-07 11:39:34 +0800

    📃 docs: Update report for Javet v3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,17 @@ object Config {
         const val COMMONS_COLLECTIONS_4 = "org.apache.commons:commons-collections4:${Versions.COMMONS_COLLECTIONS_4}"
         // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
         const val COMMONS_LANG_3 = "org.apache.commons:commons-lang3:${Versions.COMMONS_LANG_3}"
+
         const val JAVET = "com.caoccao.javet:javet:${Versions.JAVET}"
+        const val JAVET_LINUX_ARM64 = "com.caoccao.javet:javet-linux-arm64:${Versions.JAVET}"
         const val JAVET_MACOS = "com.caoccao.javet:javet-macos:${Versions.JAVET}"
-        const val JUNIT_BOM = "org.junit:junit-bom:${Versions.JUNIT}"
-        const val JUNIT_JUPITER = "org.junit.jupiter:junit-jupiter"
+
+        // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
+        const val JUNIT_JUPITER_API = "org.junit.jupiter:junit-jupiter-api:${Versions.JUNIT_JUPITER}"
+
+        // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-engine
+        const val JUNIT_JUPITER_ENGINE = "org.junit.jupiter:junit-jupiter-engine:${Versions.JUNIT_JUPITER}"
+
         const val SELF4J_API = "org.slf4j:slf4j-api:${Versions.SELF4J}"
         const val SELF4J_LOG4J_12 = "org.slf4j:slf4j-log4j12:${Versions.SELF4J}"
     }
@@ -33,8 +40,8 @@ object Config {
     object Versions {
         const val COMMONS_COLLECTIONS_4 = "4.4"
         const val COMMONS_LANG_3 = "3.12.0"
-        const val JAVET = "2.2.0"
-        const val JUNIT = "5.9.1"
+        const val JAVET = "3.0.1"
+        const val JUNIT_JUPITER = "5.10.1"
         const val SELF4J = "2.0.7"
     }
 }
@@ -53,15 +60,19 @@ repositories {
 dependencies {
     implementation(Config.Projects.COMMONS_LANG_3)
     implementation(Config.Projects.COMMONS_COLLECTIONS_4)
-    if (OperatingSystem.current().isMacOsX()) {
+    val os = OperatingSystem.current()
+    val cpuArch = System.getProperty("os.arch")
+    if (os.isMacOsX) {
         implementation(Config.Projects.JAVET_MACOS)
+    } else if (os.isLinux && (cpuArch == "aarch64" || cpuArch == "arm64")) {
+        implementation(Config.Projects.JAVET_LINUX_ARM64)
     } else {
         implementation(Config.Projects.JAVET)
     }
     implementation(Config.Projects.SELF4J_API)
     implementation(Config.Projects.SELF4J_LOG4J_12)
-    testImplementation(platform(Config.Projects.JUNIT_BOM))
-    testImplementation(Config.Projects.JUNIT_JUPITER)
+    testImplementation(Config.Projects.JUNIT_JUPITER_API)
+    testRuntimeOnly(Config.Projects.JUNIT_JUPITER_ENGINE)
 }
 
 tasks.test {
